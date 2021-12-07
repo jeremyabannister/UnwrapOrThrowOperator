dfd4659c127ec06446e0c054e70288b740e578e0
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2021-12-07 15:03:52 +0100

    Cleaned up `Package.swift`.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,21 +6,18 @@ import PackageDescription
 let package = Package(
     name: "UnwrapOrThrowOperator",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "UnwrapOrThrowOperator",
-            targets: ["UnwrapOrThrowOperator"]),
+            targets: ["UnwrapOrThrowOperator"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(
             url: "https://github.com/jeremyabannister/ErrorMessageType",
             from: "0.1.0"
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UnwrapOrThrowOperator",
             dependencies: ["ErrorMessageType"]
